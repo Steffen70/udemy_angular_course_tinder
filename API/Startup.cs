fe679272531755c122b5e1c02b620d667f0bd269
Steffen70 <steffen@seventy.mx>
2021-03-17 21:37:03 +0100

    Building a walking skeleton Part Two - Angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -52,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
