52c821909751ac1f260761d452949511a25f9c52
Steffen70 <steffen@seventy.mx>
2021-03-18 14:15:37 +0100

    New Home component with Register form

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+}
