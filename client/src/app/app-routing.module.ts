2f0882c1383df7af603c064fec7a5aa5222012a6
Steffen70 <steffen@seventy.mx>
2021-03-18 16:34:56 +0100

    Added an Angular route guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,23 +14,31 @@ const routes: Routes = [
   },
 
   {
-    path: 'members',
-    component: MemberListComponent
-  },
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'members',
+        component: MemberListComponent,
+        canActivate: [AuthGuard]
+      },
 
-  {
-    path: 'members/:id',
-    component: MemberDetailComponent
-  },
+      {
+        path: 'members/:id',
+        component: MemberDetailComponent
+      },
 
-  {
-    path: 'lists',
-    component: ListsComponent
-  },
+      {
+        path: 'lists',
+        component: ListsComponent
+      },
 
-  {
-    path: 'messages',
-    component: MessagesComponent
+      {
+        path: 'messages',
+        component: MessagesComponent
+      }
+    ]
   },
 
   {
