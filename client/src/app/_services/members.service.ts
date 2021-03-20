32b74bc7aed5107df40ee6ae29433ce51a37af49
Steffen70 <steffen@seventy.mx>
2021-03-20 21:56:42 +0100

    Using the members service to store state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -8,18 +10,30 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members
+      })
+    );
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + `users/${username}`);
+    const member = this.members.find(m => m.userName === username);
+    return member !== undefined ? of(member) : this.http.get<Member>(this.baseUrl + `users/${username}`);
   }
 
   updateMember(member: Member) {
-    return this.http.put<Member>(`${this.baseUrl}users`, member);
+    return this.http.put<Member>(`${this.baseUrl}users`, member).pipe(map(updatedMember => {
+      const index = this.members.indexOf(member);
+      this.members[index] = updatedMember;
+      return updatedMember;
+    }));
   }
 }
