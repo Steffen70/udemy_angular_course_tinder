9c5d24811fe081ac9629b9335f796ddbd80817cb
Steffen70 <steffen@seventy.mx>
2021-03-23 14:38:59 +0100

    Added a simple gender selector to member-list

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
@@ -13,17 +14,32 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   currentPage = 1;
   itemsPerPage = 5;
+  genderFilterForm: FormGroup;
 
-  constructor(private membersService: MembersService) { }
+  constructor(private membersService: MembersService, private fb: FormBuilder,) { }
 
   ngOnInit(): void {
     this.loadMembers();
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.genderFilterForm = this.fb.group({ gender: '' });
+    this.genderFilterForm.valueChanges.subscribe(() => this.genderFilterChanged());
+  }
+
+  genderFilterChanged() {
+    if (this.genderFilterForm.value?.gender != null && this.pagination.gender !== this.genderFilterForm.value.gender) {
+      this.pagination.gender = this.genderFilterForm.value.gender;
+      this.loadMembers();
+    }
   }
 
   loadMembers() {
     this.membersService.getMembers(this.currentPage, this.itemsPerPage).subscribe(response => {
       this.members = response.result;
-      this.pagination = response.pagiantion
+      this.pagination = response.pagiantion;
+      this.genderFilterForm.setValue({ gender: this.pagination.gender });
     })
   }
 
