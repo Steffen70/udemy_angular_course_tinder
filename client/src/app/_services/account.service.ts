079ad710dfb4589f0207e49c848bbb5e53f85d6f
Steffen70 <steffen@seventy.mx>
2021-03-28 19:43:30 +0200

    Added an admin.guard to secure the admin-panel

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -29,6 +29,9 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -36,5 +39,9 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
