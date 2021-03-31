58edace62d7947e0c49f9de3923ce70780b919ee
Steffen70 <steffen@seventy.mx>
2021-03-31 11:49:36 +0200

    Reworked message thread

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
@@ -8,16 +8,35 @@ import { MessageService } from 'src/app/_services/message.service';
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
 })
-export class MemberMessagesComponent implements OnInit {
+export class MemberMessagesComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scrollMe') scrollMe: ElementRef;
   @ViewChild('messageForm') messageForm: NgForm;
   @Input() username: string;
   messageContent: string;
+  messages: Message[] = [];
+  scroll: boolean = true;
 
   constructor(public messageService: MessageService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.messageService.messageThread$.subscribe(m => {
+      this.messages = m;
+      this.scroll = true;
+    });
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollBottom();
+  }
 
   sendMessage() {
     this.messageService.sendMessage(this.username, this.messageContent).then(() => this.messageForm.reset());
+  }
+
+  scrollBottom() {
+    if (this.scroll && this.scrollMe?.nativeElement) {
+      this.scroll = false;
+      this.scrollMe.nativeElement.scrollTop = this.scrollMe.nativeElement.scrollHeight;
+    }
   }
 }
