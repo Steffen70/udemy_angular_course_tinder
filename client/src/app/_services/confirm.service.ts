ae959b6ae82dd5afd29a6d0cdce2945d09159516
Steffen70 <steffen@seventy.mx>
2021-03-31 11:04:31 +0200

    Replaced confirmation prompt

---
+++ client/src/app/_services/confirm.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Subscriber } from 'rxjs';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmService {
+  bsModelRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
+
+  confirm(title = 'Confirmation', message = 'Are you sure you want to continue? Any unsaved changes will be lost.', btnOkText = 'Ok', btnCalcelText = 'Cancel')
+    : Observable<boolean> {
+    const config = {
+      initialState: {
+        title,
+        message,
+        btnOkText,
+        btnCalcelText
+      }
+    }
+
+    this.bsModelRef = this.modalService.show(ConfirmDialogComponent, config);
+
+    return new Observable<boolean>(this.getResult());
+  }
+
+  private getResult() {
+    return (observer: Subscriber<boolean>) => {
+      const subscription = this.bsModelRef.onHidden.subscribe(() => {
+        observer.next(this.bsModelRef.content.result);
+        observer.complete();
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      }
+    }
+  }
+}
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Message } from '../_models/message';
 import { Pagination } from '../_models/pagination';
 import { MessageParams } from '../_models/paginationParams';
+import { ConfirmService } from '../_services/confirm.service';
 import { MessageService } from '../_services/message.service';
 
 @Component({
@@ -15,7 +16,7 @@ export class MessagesComponent implements OnInit {
   messageParams: MessageParams;
   loadingFlag = false;
 
-  constructor(private messageService: MessageService) {
+  constructor(private messageService: MessageService, private confirmService: ConfirmService) {
     this.messageParams = this.messageService.getMessageParams();
   }
 
@@ -42,7 +43,11 @@ export class MessagesComponent implements OnInit {
   }
 
   deleteMessage(id: number) {
-    this.messageService.deleteMessage(id).subscribe(() =>
-      this.messages.splice(this.messages.findIndex(m => m.id == id), 1));
+    this.confirmService.confirm('Confirm delete message', 'This cannot be undone').subscribe(r => {
+      if (r) {
+        this.messageService.deleteMessage(id).subscribe(() =>
+          this.messages.splice(this.messages.findIndex(m => m.id == id), 1));
+      }
+    });
   }
 }
